1714745ecbe95378f04c2cf4861b25cd76422b31
evikramss <evikramss@gmail.com>
2024-10-09 07:50:25 +0000

    updated

--- UltraSonicTransducerSim.py
+++ UltraSonicTransducerSim.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib.ticker import AutoMinorLocator
 
 ## This class simulates 2 ultrasonic transudcer seperated by a distance (integer multiple of half wavelength)
 ## to observe the nodes formed in between them. Each source is treated as a 5-point array to get source directionality.
@@ -8,13 +9,16 @@ import matplotlib.animation as animation
 ## Code based on course - Computers, Waves, Simulations: A Practical Introduction to Numerical Methods
 class UltraSonicTransducerSim:
 
+    # If we need to test for different frequency, try varying distance_multiplier & x_size/z_size & total time
+    # working conditions for 40Khz sine wave - distance_multiplier=7, x_size & z_size = 0.2 meters, runtime = 0.1, dx = 0.0004, array_dist = 10, src_f1=src_f2=40000
+    # working conditions for 4Khz sine wave - distance_multiplier=7, x_size & z_size = 2.0 meters, runtime = 0.1, dx = 0.01, array_dist = 4.0, src_f1=src_f2=4000
     def __init__(self):
         # changable params
         self.distance_multiplier = 7.0
     
         ## wave speed params
-        self.cmax = 343.0 # max speed in m/s in air
-        self.c0 = 340.0 # speed in m/s in air
+        self.cmax = 348.5 # max speed in m/s in air
+        self.c0 = 348.5 # speed in m/s in air
         
         
         
@@ -22,6 +26,7 @@ class UltraSonicTransducerSim:
         self.x_size = 0.2 # in meter ?
         self.z_size = 0.2 # in meter ?
         self.dx = 0.0004
+        self.array_dist = int(10.0)
         
         self.nx = int(self.x_size / self.dx)      # grid points in x
         self.nz = int(self.z_size / self.dx)     # grid points in z
@@ -39,6 +44,7 @@ class UltraSonicTransducerSim:
         
         self.T = min(self.src_T1, self.src_T2)
         self.calc_dx = self.cmax * self.T / 2.0 # get grid distance required to capture wave
+        print('dx is : ' + str(self.dx) + ' and it should be less than ' + str(self.calc_dx))
         
         if self.dx > self.calc_dx:
             raise Exception('Check dx values. dx : ' + str(self.dx) + ' but required is ' + str(self.calc_dx))
@@ -56,6 +62,10 @@ class UltraSonicTransducerSim:
         self.isz1 = int (self.src_z1 / self.dx)
         self.isx2 = int(self.src_x2 / self.dx)
         self.isz2 = int (self.src_z2 / self.dx)
+        print('Distance between sources in meters :' + str(self.src_x2 - self.src_x1))
+        
+        if self.isx2 > self.nx or self.isx1 > self.nx:
+            raise Exception('Check distance_multiplier value. Value is high and source is falling outside grid')
         ## emitter params - end
         
         
@@ -88,6 +98,14 @@ class UltraSonicTransducerSim:
         
         
         
+        # Receiver value arrays
+        self.irx = self.isx2 - self.isx1 + 1
+        self.irz = self.isz1 # assume same z for both transmitters. Measuring along this axis
+        self.seis = np.zeros((self.irx, self.nt))
+        self.seisPlot = np.zeros((self.irx, self.irx))
+        
+        
+        
         # emitter wave equation
         self.src1 = self.calcsrc(self.src_f1, 15.0, 0.0)
         self.src2 = self.calcsrc(self.src_f2, 15.0, 0.0)
@@ -106,7 +124,8 @@ class UltraSonicTransducerSim:
     def run(self):
     
         # Plot preparation
-        fig = plt.figure()
+        fig, ax = plt.subplots()
+        fig_seis, ax_seis = plt.subplots()
         
         v1 = max([np.abs(self.src1.min()), np.abs(self.src1.max())])
         v2 = max([np.abs(self.src2.min()), np.abs(self.src2.max())])
@@ -123,9 +142,19 @@ class UltraSonicTransducerSim:
         plt.ylabel('iz')
         
         ani = animation.FuncAnimation(fig, self.update,  blit=False, frames = self.nt, interval = 0, repeat = False)
+        self.line, = ax.plot(np.arange(1, self.irx + 1), self.seis[:, 0], color='red')
+        ax.set_ylim(-3*v, 3*v)
+        ax.xaxis.set_minor_locator(AutoMinorLocator(5))
+        ax.grid(axis='x', which='both')
+        ax.grid(axis='y')
+        ani_seis = animation.FuncAnimation(fig_seis, self.update2,  blit=False, frames = self.nt, interval = 0, repeat = False)
         plt.show()
     
-    def update(self, it):    
+    def update2(self, it):
+        self.line.set_ydata(self.seis[:, it])
+        return self.line,
+    
+    def update(self, it):
     
         if it == self.nt - 1:
             print('done')
@@ -163,21 +192,24 @@ class UltraSonicTransducerSim:
         
         # Time extrapolation
         self.pnew = (2 * self.p) - self.pold + ((self.dt ** 2) * (self.c ** 2) * (self.pxx + self.pzz))
-        
+
         # Add source term at isx, isz - treat each source as an array to simulate directed beam
         self.pnew[self.isz1, self.isx1] = self.pnew[self.isz1, self.isx1] + self.src1[it]
-        self.pnew[self.isz1 - 10, self.isx1] = self.pnew[self.isz1, self.isx1] + self.src1[it]
-        self.pnew[self.isz1 - 20, self.isx1] = self.pnew[self.isz1, self.isx1] + self.src1[it]
-        self.pnew[self.isz1 + 10, self.isx1] = self.pnew[self.isz1, self.isx1] + self.src1[it]
-        self.pnew[self.isz1 + 20, self.isx1] = self.pnew[self.isz1, self.isx1] + self.src1[it]
+        self.pnew[self.isz1 - self.array_dist, self.isx1] = self.pnew[self.isz1, self.isx1] + self.src1[it]
+        self.pnew[self.isz1 - (2 * self.array_dist), self.isx1] = self.pnew[self.isz1, self.isx1] + self.src1[it]
+        self.pnew[self.isz1 + self.array_dist, self.isx1] = self.pnew[self.isz1, self.isx1] + self.src1[it]
+        self.pnew[self.isz1 + (2 * self.array_dist), self.isx1] = self.pnew[self.isz1, self.isx1] + self.src1[it]
         
         self.pnew[self.isz2, self.isx2] = self.pnew[self.isz2, self.isx2] + self.src2[it]
-        self.pnew[self.isz2 - 10, self.isx2] = self.pnew[self.isz2, self.isx2] + self.src2[it]
-        self.pnew[self.isz2 - 20, self.isx2] = self.pnew[self.isz2, self.isx2] + self.src2[it]
-        self.pnew[self.isz2 + 10, self.isx2] = self.pnew[self.isz2, self.isx2] + self.src2[it]
-        self.pnew[self.isz2 + 20, self.isx2] = self.pnew[self.isz2, self.isx2] + self.src2[it]
+        self.pnew[self.isz2 - self.array_dist, self.isx2] = self.pnew[self.isz2, self.isx2] + self.src2[it]
+        self.pnew[self.isz2 - (2 * self.array_dist), self.isx2] = self.pnew[self.isz2, self.isx2] + self.src2[it]
+        self.pnew[self.isz2 + self.array_dist, self.isx2] = self.pnew[self.isz2, self.isx2] + self.src2[it]
+        self.pnew[self.isz2 + (2 * self.array_dist), self.isx2] = self.pnew[self.isz2, self.isx2] + self.src2[it]
         
         self.pold, self.p = self.p, self.pnew
+        
+        # Save seismograms
+        self.seis[:, it] = np.transpose(self.p[self.irz, self.isx1 : self.isx2 + 1])
         
         self.image.set_array(self.pnew)
             
